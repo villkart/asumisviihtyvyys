4b6d35deec0858cc13d79cd78ce98607503ecbd1
latte1635 <45419453+latte1635@users.noreply.github.com>
2018-11-28 11:54:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+'use strict';
+const aside = document.querySelector('aside');
+
+function tausta() {
+  if (document.getElementById("nappi1").checked === true) {
+    aside.setAttribute('style', 'background-color: blue;');
+  }
+  else{
+    aside.setAttribute('style', 'background-color: lightblue;');
+  }
+}

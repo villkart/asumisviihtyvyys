cf8b1c9b9d76270b9a5d216b2bb50e7dbfcc2aef
epaavola <45419044+epaavola@users.noreply.github.com>
2018-11-28 12:13:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ function tausta() {
     aside.setAttribute('style', 'background-color: lightblue;');
   }
 }
+
+
+
+let kartta = L.map('map').setView([60.230, 24.897], 10);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  maxZoom: 13,
+
+  id: 'mapbox.streets'
+}).addTo(kartta);

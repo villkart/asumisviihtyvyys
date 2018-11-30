e609a03751a1e9b0d987b16d01a541d82484c810
latte1635 <45419453+latte1635@users.noreply.github.com>
2018-11-30 12:21:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 
 'use strict';
-const aside = document.querySelector('aside');
+const ul = document.querySelector('#results');
+const bot = document.querySelector('.bot');
+bot.style.display = 'none';
+const res = document.querySelector('#res');
+document.querySelector('#nappi').addEventListener('click', function() {
+  const haku = document.querySelector('#hakukentta').value;
+  fetch('http://api.tvmaze.com/search/shows?q=' + haku).then(function(vastaus) {
+    return vastaus.json();
+  }).then(function(json) {
+    showlist(json);
+  }).catch(function(error) {
+    console.log(error);
+  });
+});
 
-function tausta() {
-  if (document.getElementById("nappi1").checked === true) {
-    aside.setAttribute('style', 'background-color: blue;');
+document.addEventListener('keyup', function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.querySelector('#nappi').click();
   }
-  else{
-    aside.setAttribute('style', 'background-color: lightblue;');
+});
+
+function showlist(lista) {
+  ul.innerHTML = '';
+  for (let i = 0; i < lista.length; i++) {
+    res.style.display = 'flex';
+    bot.style.display = 'inline';
+
+    ul.innerHTML += `<li id="lis">`;
+    if (lista[i].show.image != null) {
+      console.log(lista[i].show.image.medium);
+      ul.innerHTML += `<img id="kuva" src="${lista[i].show.image.medium}">`;
+    }
+    ul.innerHTML += `<ul style="display: inline">`;
+    for (let e = 0; e < lista[i].show.genres.length; e++) {
+      console.log(lista[i].show.genres[e]);
+      ul.innerHTML += `<li style='list-style: none; font-weight: bold'>${lista[i].show.genres[e]}</li>`;
+    }
+    ul.innerHTML += `</ul>`;
+    ul.innerHTML += `<h2>${lista[i].show.name}</h2>`;
+    ul.innerHTML += `<p>${lista[i].show.summary}</p>`;
+    if (lista[i].show.officialSite != null) {
+      console.log(lista[i].show.officialSite);
+      ul.innerHTML += `<a style="border: 2px solid red;border-radius: 4px;color: black;background-color: darkred; text-decoration: none; " href="${lista[i].show.officialSite}">Official&nbsp;site</a>`;
+    }
+    ul.innerHTML += `</li>`;
   }
+
 }
-
-
-
-let kartta = L.map('map').setView([60.230, 24.897], 10);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-  maxZoom: 13,
-
-  id: 'mapbox.streets'
-}).addTo(kartta);

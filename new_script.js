65e1794e33ad39a722b96297611cf199a41d4b74
epaavola <45419044+epaavola@users.noreply.github.com>
2018-12-05 13:53:36 +0200

    Update new_script.js

--- new_script.js
+++ new_script.js
@@ -1,105 +1,375 @@
 'use strict';
-const aside = document.querySelector('aside');
-let footer = document.querySelector('footer');
-//let json = require('kuntarajat-ok.geojsonjson'); //(with path)
-//let json = $.getJSON("kuntarajat-ok.json");
-//let data = eval("(" +json.responseText + ")");
 
-function readJSON(path) {
-  fetch(path).then(function(vastaus) {
+function readAsukkaat(path, settings) {
+  console.log(settings);
+  fetch(path, settings).then(function(vastaus) {
     return vastaus.json();
   }).then(function(json) {
-    //console.log(json.features);
-    const d = json.features;
-    valmis(d);
+    console.log('vast', json);
+    valmis2(json);
   }).catch(function(error) {
     console.log(error);
   });
 }
 
-const data = readJSON('kuntarajat-ok.json');
+const data_1 = {
+  'query': [
+    {
+      'code': 'Alue 2018',
+      'selection': {
+        'filter': 'item',
+        'values': [
+          '020',
+          '005',
+          '009',
+          '010',
+          '016',
+          '018',
+          '019',
+          '035',
+          '043',
+          '046',
+          '047',
+          '049',
+          '050',
+          '051',
+          '052',
+          '060',
+          '061',
+          '062',
+          '065',
+          '069',
+          '071',
+          '072',
+          '074',
+          '075',
+          '076',
+          '077',
+          '078',
+          '079',
+          '081',
+          '082',
+          '086',
+          '111',
+          '090',
+          '091',
+          '097',
+          '098',
+          '099',
+          '102',
+          '103',
+          '105',
+          '106',
+          '108',
+          '109',
+          '139',
+          '140',
+          '142',
+          '143',
+          '145',
+          '146',
+          '153',
+          '148',
+          '149',
+          '151',
+          '152',
+          '165',
+          '167',
+          '169',
+          '170',
+          '171',
+          '172',
+          '176',
+          '177',
+          '178',
+          '179',
+          '181',
+          '182',
+          '186',
+          '202',
+          '204',
+          '205',
+          '208',
+          '211',
+          '213',
+          '214',
+          '216',
+          '217',
+          '218',
+          '224',
+          '226',
+          '230',
+          '231',
+          '232',
+          '233',
+          '235',
+          '236',
+          '239',
+          '240',
+          '320',
+          '241',
+          '322',
+          '244',
+          '245',
+          '249',
+          '250',
+          '256',
+          '257',
+          '260',
+          '261',
+          '263',
+          '265',
+          '271',
+          '272',
+          '273',
+          '275',
+          '276',
+          '280',
+          '284',
+          '285',
+          '286',
+          '287',
+          '288',
+          '290',
+          '291',
+          '295',
+          '297',
+          '300',
+          '301',
+          '304',
+          '305',
+          '312',
+          '316',
+          '317',
+          '318',
+          '398',
+          '399',
+          '400',
+          '407',
+          '402',
+          '403',
+          '405',
+          '408',
+          '410',
+          '416',
+          '417',
+          '418',
+          '420',
+          '421',
+          '422',
+          '423',
+          '425',
+          '426',
+          '444',
+          '430',
+          '433',
+          '434',
+          '435',
+          '436',
+          '438',
+          '440',
+          '441',
+          '475',
+          '478',
+          '480',
+          '481',
+          '483',
+          '484',
+          '489',
+          '491',
+          '494',
+          '495',
+          '498',
+          '499',
+          '500',
+          '503',
+          '504',
+          '505',
+          '508',
+          '507',
+          '529',
+          '531',
+          '535',
+          '536',
+          '538',
+          '541',
+          '543',
+          '545',
+          '560',
+          '561',
+          '562',
+          '563',
+          '564',
+          '309',
+          '576',
+          '577',
+          '578',
+          '445',
+          '580',
+          '581',
+          '599',
+          '583',
+          '854',
+          '584',
+          '588',
+          '592',
+          '593',
+          '595',
+          '598',
+          '601',
+          '604',
+          '607',
+          '608',
+          '609',
+          '611',
+          '638',
+          '614',
+          '615',
+          '616',
+          '619',
+          '620',
+          '623',
+          '624',
+          '625',
+          '626',
+          '630',
+          '631',
+          '635',
+          '636',
+          '678',
+          '710',
+          '680',
+          '681',
+          '683',
+          '684',
+          '686',
+          '687',
+          '689',
+          '691',
+          '694',
+          '697',
+          '698',
+          '700',
+          '702',
+          '704',
+          '707',
+          '729',
+          '732',
+          '734',
+          '736',
+          '790',
+          '738',
+          '739',
+          '740',
+          '742',
+          '743',
+          '746',
+          '747',
+          '748',
+          '791',
+          '749',
+          '751',
+          '753',
+          '755',
+          '758',
+          '759',
+          '761',
+          '762',
+          '765',
+          '766',
+          '768',
+          '771',
+          '777',
+          '778',
+          '781',
+          '783',
+          '831',
+          '832',
+          '833',
+          '834',
+          '837',
+          '844',
+          '845',
+          '846',
+          '848',
+          '849',
+          '850',
+          '851',
+          '853',
+          '857',
+          '858',
+          '859',
+          '886',
+          '887',
+          '889',
+          '890',
+          '892',
+          '893',
+          '895',
+          '785',
+          '905',
+          '908',
+          '911',
+          '092',
+          '915',
+          '918',
+          '921',
+          '922',
+          '924',
+          '925',
+          '927',
+          '931',
+          '934',
+          '935',
+          '936',
+          '941',
+          '946',
+          '976',
+          '977',
+          '980',
+          '981',
+          '989',
+          '992',
+        ],
+      },
+    },
+    {
+      'code': 'Tiedot',
+      'selection': {
+        'filter': 'item',
+        'values': [
+          'M411',
+          'M476',
+          'M297',
+          'M486',
+          'M162',
+          'M78',
+          'M485',
+        ],
+      },
+    }], 'response': {'format': 'json'},
+};
 
+const asetukset = {
+  method: 'POST', // *GET, POST, PUT, DELETE, etc.
+  body: JSON.stringify(data_1), // body data type must match "Content-Type" header
+};
 
+const asukkaat = readAsukkaat(
+    'https://pxnet2.stat.fi:443/PXWeb/api/v1/fi/Kuntien_avainluvut/2018/kuntien_avainluvut_2018_viimeisin.px',
+    asetukset);
 
-
-function valmis(data) {
-  let piste = [];
-  let v = [];
+function valmis2(data) {
 
   console.log(data);
 
-  for (let i = 0; i < data.length; i++) {
-    v = [];
-    let muoto = [];
-
-    try {
-      for (let u = 0; u < data[i].geometry.coordinates[0].length; u++) {
-
-        let m = [];
-        piste = L.GeoJSON.coordsToLatLng(data[i].geometry.coordinates[0][u]);
-        muoto.push(piste);
-        m.push(muoto[u].lat, muoto[u].lng);
-        //console.log(m);
-        v.push(m);
-      }
-
-      L.polygon(
-          v
-      ).
-          addTo(mymap).
-          bindPopup(`${data[i].properties.name}`);
-
-    } catch (e) {
-
-      for (let e = 0; e < data[i].geometry.geometries.length; e++) {
-        muoto = [];
-        v = [];
-        for (let u = 0; u <
-        data[i].geometry.geometries[e].coordinates[0].length; u++) {
-          let m = [];
-          piste = L.GeoJSON.coordsToLatLng(
-              data[i].geometry.geometries[e].coordinates[0][u]);
-          muoto.push(piste);
-          //console.log(muoto);
-          //console.log(muoto[u].lng);
-          m.push(muoto[u].lat, muoto[u].lng);
-          //console.log(m);
-          v.push(m);
-        }
-
-        L.polygon(
-            v
-        ).
-            addTo(mymap).
-            bindPopup(`${data[i].properties.name}`);
-
-      }
-    }
-
-
-
-  }
 }
 
-  /*function tausta() {
-    if (document.getElementById('nappi1').checked === true) {
-      aside.setAttribute('style', 'background-color: blue;');
-    }
-    else {
-      aside.setAttribute('style', 'background-color: lightblue;');
-    }
-  }
-  */
 
-  let mymap = L.map('map').setView([65.130, 24.897], 5);
-  L.tileLayer(
-      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets',
-      }).addTo(mymap);
+
+
+
+
+
 
 
